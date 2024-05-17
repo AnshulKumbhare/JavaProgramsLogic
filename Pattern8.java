b7d0387ec4178e0275eff9ff453239f7a197bcf1
AnshulKumbhare <anshulkumbhare16@gmail.com>
2024-05-17 16:10:51 +0530

    Pattern 8

---
+++ Pattern8.java
@@ -0,0 +1,16 @@
+public class Pattern8 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i ++){
+            for (int s = 0; s < i; s ++){
+                System.out.print(" ");
+            }
+
+
+            for (int j = 0; j < 5; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
