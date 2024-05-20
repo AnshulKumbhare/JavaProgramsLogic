028a6f4c323e5118a60a7f58a2679a183b296193
AnshulKumbhare <anshulkumbhare16@gmail.com>
2024-05-20 15:36:43 +0530

    Greatest Common Divisor

---
+++ GCD.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class GCD {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter num1: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter num2: ");
+        int b = sc.nextInt();
+
+        System.out.println(gcdOfTwoNumber(a, b));
+    }
+    
+    private static int gcdOfTwoNumber(int n1, int n2){
+        int gcd = 1;
+        int max = Math.max(n1, n2);
+        for (int i = max; i >= 1; i--){
+            if(n1 % i == 0 && n2 % i ==0){
+                gcd = i;
+                break;
+            }
+        }
+        return gcd;
+    }
+}
