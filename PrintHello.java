d2d6e91ea4e9df9302096002b39ef506c27d5561
AnshulKumbhare <anshulkumbhare16@gmail.com>
2024-05-22 10:52:01 +0530

    Print hello and Name

---
+++ PrintHello.java
@@ -0,0 +1,7 @@
+// Write a Java program to print 'Hello' on screen and your name on a separate line.
+
+public class PrintHello {
+    public static void main(String[] args) {
+        System.out.println("Hello \nAnshul Kumbhare");
+    }
+}
